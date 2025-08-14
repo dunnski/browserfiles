4bfd123337aaa04626078318979fce5890470fab
Joshua Dunn <jdunn223@proton.me>
2025-08-14 07:32:49 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+// This is a simple "Hello, World!" program in JavaScript.
+
+// The console.log() function is used to print output to the web console.
+// You can view the console in most web browsers by pressing F12 or
+// right-clicking on a webpage and selecting "Inspect".
+console.log("Hello, World!");
